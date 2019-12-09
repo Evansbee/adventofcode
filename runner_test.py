d4696415b8a6732cd652b8bf5d453afd8dff98e6
Evan Ackmann <evansbee@gmail.com>
2019-12-09 10:49:01 -0500

    more stuff

---
+++ runner_test.py
@@ -0,0 +1,24 @@
+#from Advent import advent
+from functools import wraps
+
+advent_problems = []
+def advent(year, day, problem):
+    def inner_function(function):
+        @wraps(function)
+        def wrapper(*args,**kwargs):
+            print(year,day,problem)
+            function("PROBLEM SET")
+        return wrapper
+    return inner_function
+
+
+@advent(2018, 12, 1)
+def problem1(problem_input):
+    print(problem_input)
+
+@advent(2018, 12, 2)
+def problem2(problem_input):
+    print(problem_input)
+
+for p in advent_problems:
+    p('adsf')
